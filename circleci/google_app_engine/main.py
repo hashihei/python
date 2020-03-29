37fbdcf5852c30f35a692f89315da778cd80e53d
hashihei <ma967gachapin@gmail.com>
2020-03-29 21:36:05 +0900

    update main.py

--- circleci/google_app_engine/main.py
+++ circleci/google_app_engine/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello GAE World!'
 
 
 if __name__ == '__main__':
